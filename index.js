14b6ba0d4e5b04aef7b96e3de6f795f6d558ee93
Rueben Hales <rhales@Ruebens-MacBook-Pro.local>
2022-01-27 10:59:08 -0500

    imported restaurants.json

---
+++ index.js
@@ -0,0 +1,9 @@
+//import restaurants
+const restaurants = require('./restaurants.json')
+
+console.log(restaurants[0].name)
+//connect to Firestore
+
+//creat a collection called "restaurants"
+
+//add each restaurant
